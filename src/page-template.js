b7f0d04ac49ba9cd808d0e95d09d78796ac475c6
Breedlove13 <b.breedlove@outlook.com>
2021-03-28 19:33:10 -0700

    package.json added

--- src/page-template.js
+++ src/page-template.js
@@ -1,8 +1,8 @@
-const readmeGenerator = () => {
+const readmeGenerator = (name, github) => {
 return
 `<br />
 <p align="center">
-  <a href="https://github.com/Cerberus0013/timed-quiz"
+  <a href="https://github.com/${github}">Github</a><
     <img src="./assets/images/download.jpg"  alt="Logo" width="80" height="" >
   </a>
 
@@ -11,14 +11,14 @@ return
   <p align="center">
   Timed Quiz Challenge
         <br />
-        <a href="https://github.com/Cerberus0013/timed-quiz"><strong>Explore the docs »</strong></a>
+        <a href="https://github.com/${github}">Github</a><><strong>Explore the docs »</strong></a>
         <br />
         <br />
-        <a href="https://github.com/Cerberus0013/timed-quiz">View Demo</a>
+       <a href="https://github.com/${github}">Github</a><View Demo</a>
         ·
-        <a href="https://github.com/Cerberus0013/timed-quiz">Report Bug</a>
+       <a href="https://github.com/${github}">Github</a><Report Bug</a>
         ·
-        <a href=""https://github.com/Cerberus0013/timed-quiz">Request Feature</a>
+       <a href="https://github.com/${github}">Github</a><Request Feature</a>
   </p>
 </p>
 
@@ -26,12 +26,12 @@ return
 
 <!-- TABLE OF CONTENTS -->
 <details open="open">
-  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
-  <ol>
-    <li><a href="#about-the-project">About The Project</a></li>
-    <li><a href="#project-links">Project Links</a></li>
-    <li><a href="#contact">Contact</a></li>
-  </ol>
+<summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+<ol>
+<li><a href="#about-the-project">About The Project</a></li>
+<li><a href="#project-links">Project Links</a></li>
+<li><a href="#contact">Contact</a></li>
+</ol>
 </details>
 
 
@@ -39,8 +39,7 @@ return
 <!-- ABOUT THE PROJECT -->
 ## About The Project
 
-![Screen Shot 2021-03-27 at 9 44 40 AM](https://user-images.githubusercontent.com/76604281/112727731-1ec5d700-8ee1-11eb-9fa6-00961e1a172e.png)
-
+<a href = ${screenshotURL}
 
 
 Features:
@@ -57,9 +56,9 @@ Repository Link: [https://github.com/Cerberus0013/timed-quiz](https://github.com
 <!-- CONTACT -->
 ## Contact
 
-Brian Breedlove - brianbreedlove0013@gmail.com`
+Name: ${name} ; Email: ${email} 
 
-
+`
 }
 
 
